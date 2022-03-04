7d4b43f49a5bc40ee232581ec54f23bff21e5ad2
gene222-arch <genephillip222@gmail.com>
2022-03-04 17:00:01 +0800

    refactor: remove unused imports

--- app/Http/Controllers/Api/MessageAdminController.php
+++ app/Http/Controllers/Api/MessageAdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MessageAdminRequest;
 use App\Mail\MessageAdmin;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MessageAdminController extends Controller
