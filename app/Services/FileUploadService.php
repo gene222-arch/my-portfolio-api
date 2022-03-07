ca718b1a3d930eccc9349b60f97393844212ee5b
gene222-arch <genephillip222@gmail.com>
2022-03-07 11:35:27 +0800

    feat: install league/flysystem-aws-s3-v3

---
+++ app/Services/FileUploadService.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+
+class FileUploadService 
+{
+    public function upload(string $key, string $directory = ''): string
+    {
+        $path = '';
+        
+        if (request()->hasFile($key))
+        {
+            $file = request()->file($key);
+
+            $originalFilename = $file->getClientOriginalName();
+            $extension = $file->getClientOriginalExtension();
+            $fileName = pathinfo($originalFilename, PATHINFO_FILENAME);
+
+            $path = $directory . $fileName .'-'. time() . ".${extension}";
+            
+            Storage::disk('s3')->put($path, $file);
+        }
+
+        return Storage::disk('s3')->url($path);
+    }
+}
