dd66a55342d8c29e1b87377fa29ab1d08dd8595a
gene222-arch <genephillip222@gmail.com>
2022-03-28 05:39:23 +0800

    feat: implement accessor to created_at attribute

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Email extends Model
 {
@@ -16,4 +18,14 @@ class Email extends Model
         'email',
         'message'
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->diffForHumans(),
+        );
+    }
 }
