b647d034b5c3ee091f865e787ac647f437770687
gene222-arch <genephillip222@gmail.com>
2022-04-10 06:40:42 +0800

    feat: implement shouldqueue mailadmin

--- app/Mail/MailAdmin.php
+++ app/Mail/MailAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MailAdmin extends Mailable
+class MailAdmin extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
